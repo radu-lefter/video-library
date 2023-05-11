f2a9c2d15ce20bc1416ddf6248271f19a2acd022
raduubraduu <lefter_radu@yahoo.com>
2023-05-11 13:47:56 +0100

    added inputs to sign in page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,11 @@
 import Input from "@/components/Input";
+import { useState } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -11,7 +16,27 @@ const Auth = () => {
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
             <div className="flex flex-col gap-4">
-              <Input />
+              <Input
+                id="name"
+                type="text"
+                label="Username"
+                value={name}
+                onChange={(e: any) => setName(e.target.value)}
+              />
+              <Input
+                id="email"
+                type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
             </div>
           </div>
         </div>
