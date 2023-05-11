830906a46bf0f8533f1239e8938295a1331435d8
raduubraduu <lefter_radu@yahoo.com>
2023-05-11 13:38:08 +0100

    created input component

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,5 @@
+import Input from "@/components/Input";
+
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -8,10 +10,10 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4">
-            
         </div>
       </div>
     </div>
