2b7fc1d14a541bafb53c69a9368320d523b0cfa7
raduubraduu <lefter_radu@yahoo.com>
2023-05-12 22:50:05 +0100

    fixed loggin issues

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,65 +1,73 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import Credentials from 'next-auth/providers/credentials';
-import prismadb from '@/libs/prismadb';
-import { compare } from 'bcrypt';
+import NextAuth, { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider  from "next-auth/providers/credentials";
+import prismadb from "@/libs/prismadb";
+import { compare } from "bcrypt";
 
-export default NextAuth({
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_ID || '',
-        clientSecret: process.env.GITHUB_SECRET || '',
-      }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID || '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      }),
-        Credentials({
-            id: 'credentials',
-            name: 'Credentials',
-            credentials: {
-              email: {
-                label: 'Email',
-                type: 'text',
-              },
-              password: {
-                label: 'Password',
-                type: 'passord'
-              }
-            },
-            async authorize(credentials){
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error('Email and password required');
-                  }
-
-                const user = await prismadb.user.findUnique({ where: {
-                email: credentials.email
-                }});
-
-                if (!user || !user.hashedPassword) {
-                throw new Error('Email does not exist');
-                }
-        
-                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
-        
-                if (!isCorrectPassword) {
-                throw new Error('Incorrect password');
-                }
-        
-                return user;
-            }
-        })
-    ],
-    pages: {
-        signIn: '/auth'
-    },
-    debug: process.env.NODE_ENV === 'development',
-    adapter: PrismaAdapter(prismadb),
-    jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET,
+export const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "passord",
+        },
       },
-    secret: process.env.NEXTAUTH_SECRET
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
 
-});
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prismadb),
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
