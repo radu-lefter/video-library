096bc5f89f90daa2a99387e913757ff7aa4250ca
raduubraduu <lefter_radu@yahoo.com>
2023-05-13 15:30:27 +0100

    created dynamic backgornd on scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,30 @@ import {BsChevronDown, BsBell, BsSearch } from 'react-icons/bs'
 import MobileMenu from '@/components/MobileMenu';
 import AccountMenu from './AccountMenu';
 
+const TOP_OFFSET = 66;
+
 const Navbar = () => {
 
     const [showMobileMenu, setShowMobileMenu] = useState(false);
     const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const [showBackground, setShowBackground] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          console.log(window.scrollY)
+          if (window.scrollY >= TOP_OFFSET) {
+            setShowBackground(true)
+          } else {
+            setShowBackground(false)
+          }
+        }
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        }
+      }, []);
 
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((current) => !current);
@@ -19,7 +39,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+      <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
         <img src="/images/logo.jpg" className="h-4 lg:h-7" alt="Logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
           <NavbarItem label="Home" active />
