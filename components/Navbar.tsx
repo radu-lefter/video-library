7be705ed55452c107e0340ea013c7af1adf867e5
raduubraduu <lefter_radu@yahoo.com>
2023-05-13 15:18:58 +0100

    created func to show mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import NavbarItem from "./NavbarItem";
 import {BsChevronDown } from 'react-icons/bs'
 import MobileMenu from '@/components/MobileMenu';
 
 const Navbar = () => {
+
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current);
+      }, []);
+
   return (
     <nav className="w-full fixed z-40">
       <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
@@ -15,10 +23,10 @@ const Navbar = () => {
           <NavbarItem label="My List" />
           <NavbarItem label="Browse by Languages" />
         </div>
-        <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
           <p className="text-white text-sm">Browse</p>
           <BsChevronDown className="text-white transition"  />
-          <MobileMenu visible />
+          <MobileMenu visible={showMobileMenu} />
         </div>
       </div>
     </nav>
