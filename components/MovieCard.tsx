b57c5160a8b4f1bb1b5574ef569ed3289b59ccaf
raduubraduu <lefter_radu@yahoo.com>
2023-05-14 22:19:46 +0100

    created func to favourite movie

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import { PlayIcon } from '@heroicons/react/24/solid';
+import FavoriteButton from '@/components/FavoriteButton';
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -69,6 +70,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             <div onClick={redirectToWatch} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
               <PlayIcon className="text-black w-4 lg:w-6" />
             </div>
+            <FavoriteButton movieId={data.id} />
             <div onClick={() => {}} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
               <ChevronDownIcon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
             </div>
