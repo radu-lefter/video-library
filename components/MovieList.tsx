f3d6884dea8e94c1ca675d08b031ef75e5f964a1
raduubraduu <lefter_radu@yahoo.com>
2023-05-14 19:27:04 +0100

    created the movie card component

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import MovieCard from '@/components/MovieCard';
 import { isEmpty } from 'lodash';
 
 interface MovieListProps {
@@ -18,7 +18,7 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">{title}</p>
         <div className="grid grid-cols-4 gap-2">
           {data.map((movie) => (
-            <div key={movie.id}>movies</div>
+            <MovieCard key={movie.id} data={movie} />
           ))}
         </div>
       </div>
